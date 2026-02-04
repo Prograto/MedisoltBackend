e8ce05a54b8221f593e9b3a81bc769868d22fcf9
Arasavalli chandra sekhar <chanduarasavalli95@gmail.com>
2026-02-04 14:37:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="MediSlot API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://medi-slot-pro.vercel.app/"],
+    allow_origins=["https://medi-slot-pro.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
